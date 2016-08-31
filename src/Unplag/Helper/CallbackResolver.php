e8bf472a17ef1cbb6e6d8c3c1855d29a100ab0c7
Maksym Tymchyk <m.tymchyk@p1k.co.uk>
2016-08-31 19:50:20 +0300

    Integrate Collback Resolver

--- src/Unplag/Helper/CallbackResolver.php
+++ src/Unplag/Helper/CallbackResolver.php
@@ -1,4 +1,6 @@
 <?php namespace Unplag\Helper;
+use Unplag\Exception\CallbackException;
+use Unplag\Response;
 
 /**
  * Class CallbackResolver
@@ -11,28 +13,64 @@ class CallbackResolver
      */
     protected $content;
 
+
+    /**
+     * @var array
+     */
+    protected $params = [];
+
     /**
      * CallbackResolver constructor.
      */
     public function __construct()
     {
-
-        foreach ($_SERVER as $name => $value)
+        $this->content = file_get_contents("php://input");
+        if( empty($this->content) )
         {
-            echo "$name: $value\n";
+            throw new CallbackException('Collback content body is Empty');
         }
 
-        $this->content = file_get_contents("php://input");
-        $this->setParams($this->content);
+        $contentType = $_SERVER['CONTENT_TYPE'];
+        if( strpos(Response::ACCEPT_MIME, $contentType) )
+        {
+            $this->setParams($this->content);
+        }
+        else
+        {
+            throw new CallbackException('Invalid MIME type');
+        }
+
     }
 
     /**
      * Method setParams description.
      * @param $content
+     *
+     * @return $this
      */
     protected function setParams($content)
     {
-
+        $unpacker = new \MessagePack\Unpacker();
+        $this->params = $unpacker->unpack($content);
+        return $this;
     }
+
+    /**
+     * Method getParam description.
+     * @param null $index
+     * @param null $default
+     *
+     * @return array|mixed|null
+     */
+    public function getParam($index = null, $default = null)
+    {
+        if( empty($index) )
+        {
+            return $this->params;
+        }
+        
+        return isset($this->params[$index]) ? $this->params[$index] : $default;
+    }
+
 
 }
